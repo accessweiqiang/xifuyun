4b9621da35db4524eab4ce1d24f4f3fa4e1eb078
weiqiang <weiqiang@buestc.com>
2016-03-24 20:55:42 +0800

    增加页面

--- res/js/dev/common.js
+++ res/js/dev/common.js
@@ -1,11 +1,24 @@
 //系统界面初始化
-$(document).ready(function(e){
-	//展开折叠
-	$(document).on("click","#platform-sidebar-wrap .sidebar-nav-title",function(){
-		var $self = $(this);
-		var $nextDom = $self.next(".sidebar-nav-list");
-		$self.toggleClass("active");
-		$nextDom.toggle();  
-	})
+$(document).ready(function(e) {
+        //展开折叠
+        $(document).on("click", "#platform-sidebar-wrap .sidebar-nav-title", function() {
+            var $self = $(this);
+            var $nextDom = $self.next(".sidebar-nav-list");
+            $self.toggleClass("active");
+            $nextDom.toggle();
+        })
+        $(document).on("click", "#platform-sidebar-wrap .sidebar-nav-item", function() {
+            var $self = $(this);
+            $("#platform-sidebar-wrap").find(".sidebar-nav-item").removeClass("active");
+            $self.toggleClass("active");
+
+        })
+
+        //初始化日期时间控件
+        $(".date-control").datepicker({
+            inline: true
+        });
+
+
 })
-//侧边栏折叠效果
+    //侧边栏折叠效果
